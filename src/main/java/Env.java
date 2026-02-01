d223da0d38ca19e64ea00e0b5c53da164acf8a77
Antonis Kosmidis <path4fe@yahoo.gr>
2026-02-01 18:58:01 +0200

    BIG_ERG -> DONE: Move light a bit to the left

--- src/main/java/Env.java
+++ src/main/java/Env.java
@@ -9,7 +9,7 @@ public class Env extends EnvironmentDescription
     public Env()
     {
         light1IsOn = true;
-        light1SetPosition(-3, 2, -7);
+        light1SetPosition(-4, 2, -7);
         add(new Box(new Vector3d(0,0,7), new Vector3f(14,1,1),this));
         add(new Box(new Vector3d(-2,0,3), new Vector3f(12,1,1),this));
         add(new Box(new Vector3d(-1.5,0,-1), new Vector3f(6,1,1),this));
